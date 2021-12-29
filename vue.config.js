e852b546f952145323c2ae2c95044cd33b0b59e5
tatsuya0903 <tama.and.pochi.0903@gmail.com>
2021-12-29 23:28:25 +0900

    GenerateSW設定 - skipWaiting: true

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { GenerateSW } = require('workbox-webpack-plugin')
+
 module.exports = {
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
   transpileDependencies: ['vuetify'],
+  configureWebpack: (config) => {
+    config.plugins.push(
+      new GenerateSW({
+        skipWaiting: true,
+      }),
+    )
+  },
 }
